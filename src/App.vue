7056830d830a8ac61c7d2b20609a3ffd1e246b3d
Vadym Kononenko <vadymk95@gmail.com>
2021-12-04 14:18:10 +0200

    master/basic setup Vue

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="app">
+    <form @submit.prevent>
+      <h4>Создание поста</h4>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="Название"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        placeholder="описание"
+      />
+      <button class="btn" @click="createPost">Создать</button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <p><strong>Название:</strong> {{ post.title }}</p>
+      <p><strong>Описание:</strong> {{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'JavaScript', body: 'Описание поста' },
+        { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
+        { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
+      ],
+      title: '',
+      body: '',
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: new Date(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+.input {
+  border: 1px solid teal;
+  width: 100%;
+  margin-top: 15px;
+  padding: 10px 15px;
+}
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background-color: transparent;
+  border: 1px solid teal;
+  color: teal;
+}
+
+.btn:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.btn:active {
+  background-color: rgba(0, 0, 0, 0.3);
+}
+</style>
